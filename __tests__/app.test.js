d5d655490193aba1c1be5d054190d8428927d358
Gabriel Simek <gabrielsimek@outlook.com>
2021-05-12 18:16:31 -0700

    all tests passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,111 @@
 import app from '../lib/app.js';
 import supertest from 'supertest';
+import { formatWeather, formatLocation, formatYelp } from '../lib/munge-utils.js';
+import weatherData from '../data/weatherbit.js';
+import locationsData from '../data/location.js';
+import yelpData from '../data/yelp.js';
 
 
-const request = supertest(app);
+// const request = supertest(app);
 
-describe('API Routes', () => {
+describe('Data Wrangling', () => {
+  const expectedWeather = [
+    {
+      'forecast': 'Broken clouds',
+      'time': '2021-05-12'
+    },
+    {
+      'forecast': 'Few clouds',
+      'time': '2021-05-13'
+    },
+    {
+      'forecast': 'Scattered clouds',
+      'time': '2021-05-14'
+    }
+     
+  ];
+
+  const expectedLocations = [
+    {
+      'formatted_query': 'Zurich, District Zurich, Zurich, Switzerland',
+      'latitude': '47.3744489',
+      'longitude': '8.5410422'
+    },
+    {
+      'formatted_query': 'Zurich, Switzerland',
+      'latitude': '47.4133024',
+      'longitude': '8.656394'
+    },
+    {
+      'formatted_query': 'Zurich, Rooks County, Kansas, USA',
+      'latitude': '39.234454',
+      'longitude': '-99.438161'
+    }
+  
+  ];
+  
+  const expectedYelpData = [
+    {
+      'name': 'Hiltl',
+      'image_url': 'https://s3-media1.fl.yelpcdn.com/bphoto/wEC0Zexz4gfFm2yN4Petgg/o.jpg',
+      'price': '$$',
+      'rating': 4.5,
+      'url': 'https://www.yelp.com/biz/hiltl-z%C3%BCrich?adjust_creative=BooKEsdi_dk8wQe0ad1HVA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=BooKEsdi_dk8wQe0ad1HVA'
+    },
+    {
+      'name': 'Sprüngli',
+      'image_url': 'https://s3-media2.fl.yelpcdn.com/bphoto/SoXofJMA5ldnfWMRZhR0cA/o.jpg',
+      'price': '$$$',
+      'rating': 4.5,
+      'url': 'https://www.yelp.com/biz/spr%C3%BCngli-z%C3%BCrich-12?adjust_creative=BooKEsdi_dk8wQe0ad1HVA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=BooKEsdi_dk8wQe0ad1HVA'
+    },
+   
+    {
+      'name': 'Zeughauskeller',
+      'image_url': 'https://s3-media2.fl.yelpcdn.com/bphoto/DY61D8R-m6u9HwWfExrhwA/o.jpg',
+      'price': '$$',
+      'rating': 4.0,
+      'url': 'https://www.yelp.com/biz/zeughauskeller-z%C3%BCrich?adjust_creative=BooKEsdi_dk8wQe0ad1HVA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=BooKEsdi_dk8wQe0ad1HVA'
+    }
+   
+  ];
+
+  
+  it('munges weather data', async () => {
+    // arrange
+    // expected is in variable above
+    // movieData is imported from file
+
+    // act 
+    const output = formatWeather(weatherData);
+
+    // assert
+    expect(output).toEqual(expectedWeather);
+  });
+
+  it('munges location data', async () => {
+    // arrange
+    // expected is in variable above
+    // movieData is imported from file
+
+    // act 
+    const output = formatLocation(locationsData);
+
+    // assert
+    expect(output).toEqual(expectedLocations);
+  });
+
+  it('munges yelp data', async () => {
+    // arrange
+    // expected is in variable above
+    // movieData is imported from file
+
+    // act 
+    const output = formatYelp(yelpData);
+
+    // assert
+    expect(output).toEqual(expectedYelpData);
+  });
+
 
 });
---
+++ data/location.js
@@ -0,0 +1,59 @@
+export default [
+  {
+    'place_id': '235521050',
+    'licence': 'https://locationiq.com/attribution',
+    'osm_type': 'relation',
+    'osm_id': '1682248',
+    'boundingbox': [
+      '47.3202203',
+      '47.4346662',
+      '8.4480061',
+      '8.6254413'
+    ],
+    'lat': '47.3744489',
+    'lon': '8.5410422',
+    'display_name': 'Zurich, District Zurich, Zurich, Switzerland',
+    'class': 'place',
+    'type': 'city',
+    'importance': 0.7851374456657509,
+    'icon': 'https://locationiq.org/static/images/mapicons/poi_place_city.p.20.png'
+  },
+  {
+    'place_id': '232455537',
+    'licence': 'https://locationiq.com/attribution',
+    'osm_type': 'relation',
+    'osm_id': '1690227',
+    'boundingbox': [
+      '47.1594376',
+      '47.6944722',
+      '8.3576804',
+      '8.9849407'
+    ],
+    'lat': '47.4133024',
+    'lon': '8.656394',
+    'display_name': 'Zurich, Switzerland',
+    'class': 'boundary',
+    'type': 'administrative',
+    'importance': 0.683306572706895,
+    'icon': 'https://locationiq.org/static/images/mapicons/poi_boundary_administrative.p.20.png'
+  },
+  {
+    'place_id': '236057138',
+    'licence': 'https://locationiq.com/attribution',
+    'osm_type': 'relation',
+    'osm_id': '129836',
+    'boundingbox': [
+      '39.228492',
+      '39.235409',
+      '-99.439019',
+      '-99.428784'
+    ],
+    'lat': '39.234454',
+    'lon': '-99.438161',
+    'display_name': 'Zurich, Rooks County, Kansas, USA',
+    'class': 'place',
+    'type': 'city',
+    'importance': 0.517511672002615,
+    'icon': 'https://locationiq.org/static/images/mapicons/poi_place_city.p.20.png'
+  }
+];
